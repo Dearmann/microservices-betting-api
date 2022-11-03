d5846484e0ddd8070089ad0beda84c09b98533a9
Dearmann <duszak0@gmail.com>
2022-11-04 00:13:49 +0100

    Added global exception handler

---
+++ match-service/src/main/java/com/github/dearmann/matchservice/exception/BadPathVariableException.java
@@ -0,0 +1,8 @@
+package com.github.dearmann.matchservice.exception;
+
+public class BadPathVariableException extends RuntimeException {
+
+    public BadPathVariableException(String message) {
+        super(message);
+    }
+}
