14d6d6b68f468bc03a9ac6f4dbf479516ed7f531
Dearmann <duszak0@gmail.com>
2022-11-05 17:42:33 +0100

    Service init - bet, comment, rate

---
+++ bet-service/src/test/java/com/github/dearmann/betservice/BetServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.dearmann.betservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BetServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
