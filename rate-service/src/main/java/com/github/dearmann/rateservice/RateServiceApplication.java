9ef6169f9b858ccf2c3c975f65a0778d941f1d6e
Dearmann <duszak0@gmail.com>
2022-11-10 19:16:37 +0100

    Discovery client setup

--- rate-service/src/main/java/com/github/dearmann/rateservice/RateServiceApplication.java
+++ rate-service/src/main/java/com/github/dearmann/rateservice/RateServiceApplication.java
@@ -2,8 +2,10 @@ package com.github.dearmann.rateservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
+@EnableEurekaClient
 public class RateServiceApplication {
 
 	public static void main(String[] args) {
