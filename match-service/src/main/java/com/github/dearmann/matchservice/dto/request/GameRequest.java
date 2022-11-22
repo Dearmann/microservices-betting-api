19b781c379a87c2e71147ffec50390bd9c0e34bd
Dearmann <duszak0@gmail.com>
2022-11-22 12:34:52 +0100

    Game request validation

--- match-service/src/main/java/com/github/dearmann/matchservice/dto/request/GameRequest.java
+++ match-service/src/main/java/com/github/dearmann/matchservice/dto/request/GameRequest.java
@@ -5,11 +5,14 @@ import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import javax.validation.constraints.NotBlank;
+
 @Data
 @Builder
 @NoArgsConstructor
 @AllArgsConstructor
 public class GameRequest {
+    @NotBlank(message = "Name is mandatory")
     private String name;
     private String logoUrl;
 }
