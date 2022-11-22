adc0a335dc9b89911a429527a9766ad48b3b7f46
Dearmann <duszak0@gmail.com>
2022-11-22 12:35:04 +0100

    Team request validation

--- match-service/src/main/java/com/github/dearmann/matchservice/dto/request/TeamRequest.java
+++ match-service/src/main/java/com/github/dearmann/matchservice/dto/request/TeamRequest.java
@@ -5,12 +5,17 @@ import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+
 @Data
 @Builder
 @NoArgsConstructor
 @AllArgsConstructor
 public class TeamRequest {
+    @NotBlank(message = "Name is mandatory")
     private String name;
     private String logoUrl;
+    @NotNull(message = "Game ID is mandatory")
     private Long gameId;
 }
