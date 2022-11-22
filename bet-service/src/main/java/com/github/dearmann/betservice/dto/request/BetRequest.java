a67eab0f4bb0fc1b916301190ff2adce56e452b3
Dearmann <duszak0@gmail.com>
2022-11-22 12:24:06 +0100

    Bet request validation

--- bet-service/src/main/java/com/github/dearmann/betservice/dto/request/BetRequest.java
+++ bet-service/src/main/java/com/github/dearmann/betservice/dto/request/BetRequest.java
@@ -5,12 +5,18 @@ import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+
 @Data
 @Builder
 @NoArgsConstructor
 @AllArgsConstructor
 public class BetRequest {
+    @NotBlank(message = "User ID is mandatory")
     private String userId;
+    @NotNull(message = "Match ID is mandatory")
     private Long matchId;
+    @NotNull(message = "Predicted team ID is mandatory")
     private Long predictedTeamId;
 }
