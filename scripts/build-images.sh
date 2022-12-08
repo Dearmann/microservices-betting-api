7fda1138bc481e1f8c4f6de4395bc61c0459883a
Dearmann <duszak0@gmail.com>
2022-12-08 07:37:56 +0100

    Image rename

--- scripts/build-images.sh
+++ scripts/build-images.sh
@@ -1,9 +1,9 @@
 cd ../
 mvn package -D"maven.test.skip=true"
-docker build -f . -t api-gateway:1.0 ./api-gateway
-docker build -f . -t bet-service:1.0 ./bet-service
-docker build -f . -t comment-service:1.0 ./comment-service
-docker build -f . -t discovery-server:1.0 ./discovery-server
-docker build -f . -t match-service:1.0 ./match-service
-docker build -f . -t rate-service:1.0 ./rate-service
-docker build -f . -t user-service:1.0 ./user-service
+docker build -f . -t dearmann/api-gateway:1.0 ./api-gateway
+docker build -f . -t dearmann/bet-service:1.0 ./bet-service
+docker build -f . -t dearmann/comment-service:1.0 ./comment-service
+docker build -f . -t dearmann/discovery-server:1.0 ./discovery-server
+docker build -f . -t dearmann/match-service:1.0 ./match-service
+docker build -f . -t dearmann/rate-service:1.0 ./rate-service
+docker build -f . -t dearmann/user-service:1.0 ./user-service
