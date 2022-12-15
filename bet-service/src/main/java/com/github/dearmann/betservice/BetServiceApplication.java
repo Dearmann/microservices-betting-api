110b65820979a8094f341013bb05ac755f73fac0
Dearmann <duszak0@gmail.com>
2022-12-15 09:46:42 +0100

    Kubernetes setup

--- bet-service/src/main/java/com/github/dearmann/betservice/BetServiceApplication.java
+++ bet-service/src/main/java/com/github/dearmann/betservice/BetServiceApplication.java
@@ -2,10 +2,8 @@ package com.github.dearmann.betservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
-@EnableEurekaClient
 public class BetServiceApplication {
 
 	public static void main(String[] args) {
--- match-service/src/main/java/com/github/dearmann/matchservice/config/WebClientConfigK8s.java
+++ match-service/src/main/java/com/github/dearmann/matchservice/config/WebClientConfigK8s.java
@@ -1,17 +1,15 @@
-package com.github.dearmann.userservice.config;
+package com.github.dearmann.matchservice.config;
 
-import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
 import org.springframework.web.reactive.function.client.WebClient;
 
 @Configuration
-@Profile("!k8s")
-public class WebClientConfig {
+@Profile("k8s")
+public class WebClientConfigK8s {
 
     @Bean
-    @LoadBalanced
     public WebClient.Builder webClientBuilder() {
         return WebClient.builder();
     }
