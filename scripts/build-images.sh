b1767cf00925738815c095d072285d8a636df724
Dearman <duszak0@gmail.com>
2023-06-05 22:45:09 +0200

    api-gateway iss claim configuration

--- scripts/build-images.sh
+++ scripts/build-images.sh
@@ -1,9 +1,9 @@
 cd ../
 mvn package -D"maven.test.skip=true"
-docker build -f . -t dearmann/api-gateway:1.1 ./api-gateway
-docker build -f . -t dearmann/bet-service:1.2 ./bet-service
-docker build -f . -t dearmann/comment-service:1.2 ./comment-service
-docker build -f . -t dearmann/discovery-server:1.0 ./discovery-server
-docker build -f . -t dearmann/match-service:1.3 ./match-service
-docker build -f . -t dearmann/rate-service:1.2 ./rate-service
-docker build -f . -t dearmann/user-service:1.2 ./user-service
+docker build -f ./Dockerfile -t dearmann/api-gateway:1.2 ./api-gateway
+docker build -f ./Dockerfile -t dearmann/bet-service:1.2 ./bet-service
+docker build -f ./Dockerfile -t dearmann/comment-service:1.2 ./comment-service
+docker build -f ./Dockerfile -t dearmann/discovery-server:1.0 ./discovery-server
+docker build -f ./Dockerfile -t dearmann/match-service:1.3 ./match-service
+docker build -f ./Dockerfile -t dearmann/rate-service:1.2 ./rate-service
+docker build -f ./Dockerfile -t dearmann/user-service:1.2 ./user-service
